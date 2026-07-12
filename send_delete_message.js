ecc368653d899e37bdbfb4b4ef4c7b3e6b6b82ab
Kubutek Programmer <kubutek@pseudonim.pl>
2026-07-13 01:03:05 +0200

    Update send_delete_message.js
    probably worst functions i wrote
    if it works, dont touch it

--- send_delete_message.js
+++ send_delete_message.js
@@ -1,7 +1,7 @@
 YTToken = /* put your YouTube authorization token here */;
 
-function sendMessage(text) {
-    fetch('https://www.youtube.com/youtubei/v1/live_chat/send_message?prettyPrint=false', {
+async function sendMessage(text) {
+    return fetch('https://www.youtube.com/youtubei/v1/live_chat/send_message?prettyPrint=false', {
         headers: {
             authorization: YTToken,
         },
@@ -12,7 +12,7 @@ function sendMessage(text) {
                     clientVersion: '2.20260630.03.00'
                 }
             },
-            params: 'Q2lrcUp3b1lWVU5wVjJVMldEYzBiV3RCZVd3d1NXcG9WRWRUYmxCUkVndEdVR0kyWkZJeFdVaEVNQkFCR0FRJTNE', // put the live chat continuation, you can get this by copying what YouTube sends through DevTools Network tab
+            params: 'Q2lrcUp3b1lWVU5wVjJVMldEYzBiV3RCZVd3d1NXcG9WRWRUYmxCUkVndEpZemRYWlZOTFMydEZjeEFCR0FFJTNE', // put the live chat continuation, you can get this by copying what YouTube sends through DevTools Network tab
             richMessage: {
                 textSegments: [
                     {
@@ -22,7 +22,7 @@ function sendMessage(text) {
             }
         }),
         method: 'POST',
-    });
+    }).then((res) => res.json());
 }
 
 function sendDeleteMessage(text) {
@@ -36,30 +36,7 @@ function sendDeleteMessage(text) {
         params: undefined
     };
 
-    fetch('https://www.youtube.com/youtubei/v1/live_chat/send_message?prettyPrint=false', {
-        headers: {
-            authorization: YTToken,
-        },
-        body: JSON.stringify({
-            context: {
-                client: {
-                    clientName: 'WEB',
-                    clientVersion: '2.20260630.03.00'
-                }
-            },
-            params: 'Q2lrcUp3b1lWVU5wVjJVMldEYzBiV3RCZVd3d1NXcG9WRWRUYmxCUkVndG9aakJ1V1UxWmRWVTBXUkFCR0FRJTNE', // put the live chat continuation, you can get this by copying what YouTube sends through DevTools Network tab
-            richMessage: {
-                textSegments: [
-                    {
-                        text
-                    }
-                ]
-            }
-        }),
-        method: 'POST',
-    }).then((res) => {
-        return res.json();
-    }).then((jsonRes) => {
+    sendMessage(text).then((jsonRes) => {
         console.log(jsonRes);
 
         jsonBody.params = jsonRes.actions[0].addChatItemAction.item.liveChatTextMessageRenderer.inlineActionButtons[0].buttonRenderer.serviceEndpoint.moderateLiveChatEndpoint.params;
@@ -75,4 +52,56 @@ function sendDeleteMessage(text) {
 
 fetch(jsonRes.actions[0].addChatItemAction.item.liveChatTextMessageRenderer.inlineActionButtons[0].buttonRenderer.serviceEndpoint.commandMetadata.webCommandMetadata.apiUrl, json);
     });
+}
+
+function massSendMessage(text, amount) {
+    for (let i = 0; i < amount; i ++) {
+        let jsonBody = {
+            context: {
+                client: {
+                    clientName: 'WEB',
+                    clientVersion: '2.20260630.03.00'
+                }
+            },
+            params: undefined
+        };
+        
+        sendMessage(text).then((jsonRes) => {
+            console.log(`message number ${i+1} sent`);
+        });
+    }
+}
+
+function massDeleteMessage(text, amount) {
+    for (let i = 0; i < amount; i ++) {
+        let jsonBody = {
+            context: {
+                client: {
+                    clientName: 'WEB',
+                    clientVersion: '2.20260630.03.00'
+                }
+            },
+            params: undefined
+        };
+        
+        sendMessage(text).then((jsonRes) => {
+            console.log(`message number ${i+1} sent`);
+        
+            jsonBody.params = jsonRes.actions[0].addChatItemAction.item.liveChatTextMessageRenderer.inlineActionButtons[0].buttonRenderer.serviceEndpoint.moderateLiveChatEndpoint.params;
+        
+            let json = {
+                headers: {
+                    authorization: YTToken,
+                },
+                body: JSON.stringify(jsonBody),
+                method: 'GET',
+            };
+            if (jsonRes.actions[0]?.addChatItemAction?.item?.liveChatTextMessageRenderer?.inlineActionButtons[0]?.buttonRenderer?.serviceEndpoint?.commandMetadata?.webCommandMetadata?.sendPost) json.method = 'POST';
+        
+            fetch(jsonRes.actions[0].addChatItemAction.item.liveChatTextMessageRenderer.inlineActionButtons[0].buttonRenderer.serviceEndpoint.commandMetadata.webCommandMetadata.apiUrl, json).then((res) => {
+                if (res.ok)
+                    console.log(`message number ${i+1} succesfully deleted`);
+            });
+        });
+    }
 }
